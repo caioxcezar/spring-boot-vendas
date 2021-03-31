dfab232d84a19bb9a68010998d2de592cf0360e6
Caio Rezende <caiocezar50@outlook.com>
2021-03-31 14:52:32 -0300

    Aula 4. Injeção de dependências

---
+++ src/main/java/io/github/caioxcezar/repository/ClientesRepository.java
@@ -0,0 +1,11 @@
+package io.github.caioxcezar.repository;
+
+import io.github.caioxcezar.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+    public void persistir(Cliente cliente) {
+        //acessa a base e salva o cliente
+    }
+}
