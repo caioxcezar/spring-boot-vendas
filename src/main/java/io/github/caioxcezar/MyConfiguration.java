c24616f28cfc60310492670c4448f29afad937a9
Caio Rezende <caiocezar50@outlook.com>
2021-03-31 15:46:58 -0300

    Aula 8. Trabalhando com múltiplos ambientes e profiles

--- src/main/java/io/github/caioxcezar/MyConfiguration.java
+++ src/main/java/io/github/caioxcezar/MyConfiguration.java
@@ -1,9 +1,19 @@
 package io.github.caioxcezar;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("devel")
 public class MyConfiguration {
+
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("RODANDO EM DESENVOLVIMENTO");
+        };
+    }
 
 }
