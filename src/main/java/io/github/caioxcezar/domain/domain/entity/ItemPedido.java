3e74aa91592161edd992e7fb4d839c9746996657
caiocezar50@outlook.com <888GkpPpVqw5>
2021-04-27 17:47:10 -0300

    17. Criando Classes de Modelo

---
+++ src/main/java/io/github/caioxcezar/domain/domain/entity/ItemPedido.java
@@ -0,0 +1,40 @@
+package io.github.caioxcezar.domain.domain.entity;
+
+public class ItemPedido {
+    private Integer id;
+    private Cliente pedido;
+    private Produto produto;
+    private Integer quantidade;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Cliente getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Cliente pedido) {
+        this.pedido = pedido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+}
