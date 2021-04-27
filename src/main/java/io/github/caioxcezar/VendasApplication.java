47357e90f106f18e8a248e561fe3030bc7a2b754
caiocezar50@outlook.com <888GkpPpVqw5>
2021-04-27 18:16:54 -0300

    18. Salvando e recuperando Clientes

--- src/main/java/io/github/caioxcezar/VendasApplication.java
+++ src/main/java/io/github/caioxcezar/VendasApplication.java
@@ -1,5 +1,7 @@
 package io.github.caioxcezar;
 
+import io.github.caioxcezar.domain.entity.Cliente;
+import io.github.caioxcezar.domain.repositorio.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,8 +12,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean CommandLineRunner init(@Autowired Clientes clientes) {
+        return args -> {
+            clientes.salvar(new Cliente("Caio"));
+            clientes.salvar(new Cliente("Outro Cliente"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+
+            todosClientes.forEach(System.out::println);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class);
