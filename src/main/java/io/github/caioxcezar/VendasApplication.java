6f9514a8a30d3dc53faeddb7295d6ebc55a55c2f
caiocezar50@outlook.com <888GkpPpVqw5>
2021-05-03 17:52:01 -0300

    24. Query Methods

--- src/main/java/io/github/caioxcezar/VendasApplication.java
+++ src/main/java/io/github/caioxcezar/VendasApplication.java
@@ -24,30 +24,10 @@ public class VendasApplication {
             clientes.save(new Cliente("Caio"));
             clientes.save(new Cliente("Outro Cliente"));
 
-            List<Cliente> todosClientes = clientes.findAll();
-            todosClientes.forEach(System.out::println);
+            boolean existe = clientes.existsByNome("Caio");
+            System.out.println("Exite um Cliente com o nome Caio? " + existe);
 
-            System.out.println("Atualizando Clientes");
-            todosClientes.forEach(c -> {
-                c.setNome(c.getNome() + " atualizado");
-                clientes.save(c);
-            });
-            todosClientes = clientes.findAll();
-            todosClientes.forEach(System.out::println);
 
-            System.out.println("Buscando Clientes");
-            clientes.findByNomeLike("Cli").forEach(System.out::println);
-
-            System.out.println("Deletando Clientes");
-            clientes.findAll().forEach(c -> {
-                clientes.delete(c);
-            });
-            todosClientes = clientes.findAll();
-            if (todosClientes.isEmpty()){
-                System.out.println("Nenhum cliente encontrado");
-            }else{
-                todosClientes.forEach(System.out::println);
-            }
         };
     }
 
