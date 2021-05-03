6b8777185831d367611676e31ba6d88e88e116cf
caiocezar50@outlook.com <888GkpPpVqw5>
2021-05-03 17:42:45 -0300

    23. Introdução JPA repository

--- src/main/java/io/github/caioxcezar/domain/repositorio/Clientes.java
+++ src/main/java/io/github/caioxcezar/domain/repositorio/Clientes.java
@@ -1,64 +1,11 @@
 package io.github.caioxcezar.domain.repositorio;
 
 import io.github.caioxcezar.domain.entity.Cliente;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
-@Repository
-public class Clientes {
+public interface Clientes extends JpaRepository<Cliente, Integer> {
 
-    private static String INSERT = "insert into cliente (nome) values (?)";
-    private static String SELECT_ALL = "select * from cliente";
-    private static String UPDATE = "update cliente set nome = ? where id = ?";
-    private static String DELETE = "delete from cliente where id = ?";
-
-    @Autowired
-    private EntityManager entityManager;
-
-    @Transactional
-    public Cliente salvar(Cliente cliente) {
-        entityManager.persist(cliente);
-        return cliente;
-    }
-
-    @Transactional
-    public Cliente atualizar(Cliente cliente) {
-        entityManager.merge(cliente);
-        return cliente;
-    }
-
-    @Transactional
-    public void deletar(Cliente cliente){
-        if(!entityManager.contains(cliente))
-            cliente = entityManager.merge(cliente);
-        entityManager.remove(cliente);
-    }
-
-    @Transactional
-    public void deletar(Integer id) {
-        Cliente cliente = entityManager.find(Cliente.class, id);
-        deletar(cliente);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> buscarPorNome(String nome) {
-        String jpgl = "select c from Cliente c where c.nome like :nome";
-        TypedQuery<Cliente> query = entityManager.createQuery(jpgl, Cliente.class);
-        query.setParameter("nome", "%"+nome+"%");
-        return query.getResultList();
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> obterTodos() {
-        return entityManager.createQuery("from Cliente", Cliente.class).getResultList();
-    }
+    List<Cliente> findByNomeLike(String nome);
 }
