ab71560809dae458b8090b881113197ca24b73bc
caiocezar50@outlook.com <888GkpPpVqw5>
2021-06-04 19:14:15 -0300

    52. Utilizando o Controller Advice

---
+++ src/main/java/io/github/caioxcezar/rest/ApiErros.java
@@ -0,0 +1,15 @@
+package io.github.caioxcezar.rest;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ApiErros {
+    @Getter
+    private List<String> erros;
+
+    public ApiErros(String mensagemErro){
+        this.erros = Arrays.asList(mensagemErro);
+    }
+}
---
+++ src/main/java/io/github/caioxcezar/rest/controller/AppControllerAdvice.java
@@ -0,0 +1,18 @@
+package io.github.caioxcezar.rest.controller;
+
+import io.github.caioxcezar.exception.RegraNegocioException;
+import io.github.caioxcezar.rest.ApiErros;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@RestControllerAdvice
+public class AppControllerAdvice {
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErros handleRegraNegocioException(RegraNegocioException ex) {
+        String mensagemErro = ex.getMessage();
+        return new ApiErros(mensagemErro);
+    }
+}
