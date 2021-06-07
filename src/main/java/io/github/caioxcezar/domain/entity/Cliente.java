93e774afd9d63f9082d5c441452aec664f680a8b
caiocezar50@outlook.com <888GkpPpVqw5>
2021-06-07 19:58:58 -0300

    59. Validando a entidade cliente

--- src/main/java/io/github/caioxcezar/domain/entity/Cliente.java
+++ src/main/java/io/github/caioxcezar/domain/entity/Cliente.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
@@ -23,6 +24,8 @@ public class Cliente {
     @NotEmpty(message = "Campo nome é obrigatório")
     private String nome;
     @Column(name = "cpf", length = 11)
+    @NotEmpty(message = "Campo CPF é obrigatório")
+    @CPF(message = "Informe um CPF válido")
     private String cpf;
     @JsonIgnore
     @OneToMany(mappedBy = "cliente")
