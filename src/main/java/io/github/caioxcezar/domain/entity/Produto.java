ab7b2001e18e093055d88a53112685358a674790
caiocezar50@outlook.com <888GkpPpVqw5>
2021-06-07 20:43:31 -0300

    63. Inernacionalização

--- src/main/java/io/github/caioxcezar/domain/entity/Produto.java
+++ src/main/java/io/github/caioxcezar/domain/entity/Produto.java
@@ -18,9 +18,9 @@ public class Produto {
     @Column(name = "id")
     private Integer id;
     @Column(name = "descricao")
-    @NotEmpty(message = "Campo descrição obrigatório.")
+    @NotEmpty(message = "{campo.descricao.obrigatorio}")
     private String descricao;
     @Column(name = "preco_unitario")
-    @NotNull(message = "Campo preço é obrigatorio.")
+    @NotNull(message = "{campo.preco.obrigatorio}")
     private BigDecimal preco;
 }
